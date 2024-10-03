1c9177b9892d4797e83aa0ed00ceccbe50ed0541
Blendi80 <blendi.mazreku@ecal.ch>
2024-10-03 11:21:29 +0200

    exercice1

--- Grid_2/JS/app.js
+++ Grid_2/JS/app.js
@@ -11,7 +11,22 @@ export default class App {
     this.canvas.width = width;
     this.canvas.height = height;
     document.body.appendChild(this.canvas);
+    this.setGradientBackground();
   }
+  setGradientBackground() {
+    const gradient = this.ctx.createLinearGradient(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+    gradient.addColorStop(0, "#6a994e");
+    gradient.addColorStop(1, "#386641");
+
+    this.ctx.fillStyle = gradient;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
   circle(x, y, radius) {
     this.ctx.beginPath();
     this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
@@ -29,13 +44,12 @@ export default class App {
 
     for (let i = 0; i < stepX; i++) {
       for (let j = 0; j < stepY; j++) {
-        // do something
         maLettre.drawLettre(
           i * spaceX + radius,
           j * spaceY + radius,
           "B",
           55,
-          "blue"
+          "#386641"
         );
       }
     }
